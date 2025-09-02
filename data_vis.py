8aaed81a9ca64683eb2d8a736a95faaec23fbc1c
MaekobaShun <maekoba.shun.job@gmail.com>
2025-09-02 22:41:16 +0900

    変更

--- data_vis.py
+++ data_vis.py
@@ -1,15 +1,11 @@
-from plotly.subplots import make_subplots
 from scipy.stats import gaussian_kde
 from scipy.stats import skew, kurtosis
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 import warnings
-import japanize_matplotlib
 
 warnings.filterwarnings('ignore')
 
